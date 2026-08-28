96e37954d9f7ce2d71bc0518b4f17eb5a5fa52e2
pransh6778-ai <pransh6778@gmail.com>
2026-08-28 14:48:47 +0530

    Time: 759 ms (7.2%), Space: 80.1 MB (5.26%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] isPrime= new boolean[n];
+        for(int i=2; i<n; i++){
+             isPrime[i]=true;
+        }
+        for(int i=2; i<Math.sqrt(n); i++){
+            if(isPrime[i]){
+                for(int j=i*i; j<n; j+=i){
+                    isPrime[j]=false;
+                }
+            }
+        }
+        int count=0;
+        for(int i=0; i<n; i++){
+            if(isPrime[i]==true){
+                count++;
+            }
+        }
+        return count;
+    }
+}
