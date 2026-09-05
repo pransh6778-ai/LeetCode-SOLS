0937e9e916e1a9ae09d1b2485dfa9c6b8d929672
pransh6778-ai <pransh6778@gmail.com>
2026-09-05 15:48:24 +0530

    Time: 9 ms (42.49%), Space: 41.9 MB (70.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,11 +3,8 @@ class Solution {
         return fibonacci(n);
     }
     public int fibonacci(int n){
-        if(n==0){
-            return 0;
-        }
-        else if(n<=2){
-            return 1;
+        if(n<2){
+            return n;
         }else{
             return fibonacci(n-1)+fibonacci(n-2);
         }
