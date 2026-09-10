b0dde491cafd28a7500cdd044aec16263afeff55
pransh6778-ai <pransh6778@gmail.com>
2026-09-11 00:34:58 +0530

    Time: 3 ms (86.8%), Space: 52.5 MB (8.58%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n= ratings.length;
+        int[] res=new int[n];
+        int candy=0;
+        for(int i=0; i<n; i++){
+            res[i]=1;
+        }
+        for(int i=1; i<n; i++){
+            if(ratings[i]>ratings[i-1]){
+                res[i]=res[i-1]+1;
+            }
+        }
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
+                res[i]=Math.max(res[i],res[i+1]+1);
+            }
+        }
+        for(int c: res){
+            candy+=c;
+        }
+        return candy;
+    }
+}
